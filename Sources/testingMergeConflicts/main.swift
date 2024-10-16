fe6a09f8db51d16225ab4d532c5c48e5664da645
APCSP <apcsp@Cart-Mac-4.local>
2024-10-16 11:05:12 -0700

    add basic counter implementation

---
+++ Sources/testingMergeConflicts/main.swift
@@ -0,0 +1,19 @@
+/// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+class Counter {
+        var value: Int = 0
+
+
+        func increment() {
+                value += 1
+        }
+
+        func decrement() {
+                value -= 1
+        }
+}
+//code to test the counter
+var counter = Counter()
+counter.increment()
+print("Counter value after the increment: \(counterValue)")
