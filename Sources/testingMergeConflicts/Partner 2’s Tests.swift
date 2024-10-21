d6acb8588f6ea32095867e83d4d78b5cea72bf8a
IIIIlIlllll <kevin20070312@icloud.com>
2024-10-21 10:50:46 -0700

    Update Partner 2’s Tests.swift

--- Sources/testingMergeConflicts/Partner 2’s Tests.swift
+++ Sources/testingMergeConflicts/Partner 2’s Tests.swift
@@ -4,7 +4,7 @@ final class CounterTests: XCTestCase {
 func testDecrement () {
 var counter = Counter ()
 counter.decrement ()
-print ("Counter value after decrement: \ (counter.value)")
+print ("Counter value after decrement: \(counter.value)")
 XCTAssertEqual (counter.value, -1, "Counter decrement failed")
 // Print the counter value
 }
